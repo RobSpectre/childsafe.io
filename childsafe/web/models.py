2c578eb616be66c8f6e468e6cf2049c8363df6b8
Ricky Robinett <ricky.robinett@gmail.com>
2017-11-11 17:49:33 -0500

    registration and signup

--- childsafe/web/models.py
+++ childsafe/web/models.py
@@ -2,7 +2,8 @@ import uuid
 
 from django.contrib.auth.models import User
 from django.db import models
-
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class ChildsafeUser(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
@@ -14,3 +15,9 @@ class ChildsafeUser(models.Model):
     email_address = models.EmailField()
     phone_number = models.CharField(max_length=255, null=True, blank=True)
     webhook_url = models.URLField(null=True, blank=True)
+
+#@receiver(post_save, sender=User)
+#def update_user_profile(sender, instance, created, **kwargs):
+    #if created:
+    #    ChildSafeUser.objects.create(user=instance)
+    #instance.profile.save()
