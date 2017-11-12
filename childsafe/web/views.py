d65703157295dd3b859dbf4bd50e2c602e695c55
Greg Baugues <greg@baugues.com>
2017-11-12 12:18:59 -0500

    Split document into docs. Add AWS page

--- childsafe/web/views.py
+++ childsafe/web/views.py
@@ -47,10 +47,23 @@ def login_page(request):
     return render(request, 'web/login.html')
 
 
+def docs_index(request):
+    return render(request, 'web/docs/index.html')
+
+
+def docs_aws(request):
+    return render(request, 'web/docs/aws-s3.html')
+
+
+def docs_gcs(request):
+    return render(request, 'web/docs/google-cloud-storage.html')
+
+
+def docs_azure(request):
+    return render(request, 'web/docs/azure.html')
+
+
 @login_required(login_url='/login/')
 def profile(request):
     return render(request, 'web/profile.html')
 
-
-def documentation(request):
-    return render(request, 'web/documentation.html')
